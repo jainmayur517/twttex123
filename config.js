bd33ebc46751c933dcac941c13a186025bb793b8
jainmayur517 <jainmayur517@gmail.com>
2019-07-28 00:28:56 +0530

    first

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '48PgYpRNxioHn4EEZOshL4m53',
+    consumer_secret: 'h9XwMlWRTR02sUn5Y5uaKslyPKVvBWlT6SHOMkfGx356TpTbMg',
+    access_token_key: '380847007-ERwzBWHRHKCW5hKZaSQzqbspxGiGZFvZwzh5XgGO',
+    access_token_secret: 'uPvUhuiYl6bTv2oa4LEvfGsvH4sVDFoppS92H07bq4P3S'
+  }
