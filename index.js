916ae925cb0763560956ce29b73225da89bf8637
jainmayur517 <jainmayur517@gmail.com>
2019-07-30 00:38:39 +0530

    first

---
+++ index.js
@@ -0,0 +1,25 @@
+var exp= require('express');
+var app =exp();
+var mainl= require('./mainl.js');
+
+var bodyParser =require('body-parser');
+var request=require('request');
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+app.get("/",function(req,res){
+    
+    res.render('fp');
+    
+    });
+
+
+    app.get('/results',function(req,res){
+            var gh=req.query.bm;
+            var users=mainl(res,gh);
+    });
+
+    app.listen(3000,function(){
+  console.log("server started!");
+  })
--- mainl.js
+++ mainl.js
@@ -1,8 +1,8 @@
-var exp= require('express');
-var app =exp();
-require('dotenv').config();
 var Twitter = require('twitter');
+require('dotenv').config();
 
+
+module.exports=function(res,gh){
 const CONSUMER_KEY = 'TWITTER_CONSUMER_KEY';
 const CONSUMER_SECRET = 'TWITTER_CONSUMER_SECRET';
 const ACCESS_TOKEN_KEY = 'TWITTER_ACCESS_TOKEN_KEY';
@@ -19,12 +19,15 @@ var client = new Twitter({
   consumer_key: process.env[CONSUMER_KEY],
   consumer_secret: process.env[CONSUMER_SECRET],
   access_token_key: process.env[ACCESS_TOKEN_KEY],
-  access_token_secret: process.env[ACCESS_TOKEN_SECRET]
+  access_token_secret: process.env[ACCESS_TOKEN_SECRET],
 });
 
-var params = {screen_name: 'PMOindia', count: 10};
+var params = {screen_name: gh, count: 10};
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
-    console.log((tweets));
+  //res.render('web',{users: tweets});
+   // console.log((tweets));
+    res.send(tweets);
   }
 });
+}
